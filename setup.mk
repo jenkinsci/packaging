295139152a25925419e98459f9f6cf09d05937de
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-03 15:50:02 -0700

    WAR parameter should be mandatory

--- setup.mk
+++ setup.mk
@@ -1,5 +1,5 @@
 # war file to release
-export WAR:=jenkins.war
+export WAR?=$(error Required variable WAR must point to the jenkins.war file you are packaging)
 
 # sanitized version number
 export VERSION:=$(shell unzip -p "${WAR}" META-INF/MANIFEST.MF | grep Implementation-Version | cut -d ' ' -f2 | tr -d '\r' | sed -e "s/-SNAPSHOT//" | sed -e "s/-beta-.*//")
