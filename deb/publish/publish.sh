c5495c2153171720b91715df8a8e448dfd45e77a
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-04 08:29:25 -0700

    Bug fix in debian packaging

--- deb/publish/publish.sh
+++ deb/publish/publish.sh
@@ -6,7 +6,7 @@ rsync -avz "${DEB}" $PKGSERVER:$DEBDIR/
 
 D=/tmp/$$
 mkdir -p $D/binary $D/contents
-cp -R "$bin/contents" $D/contents
+cp -R "$bin/contents/." $D/contents
 
 # build package index
 # see http://wiki.debian.org/SecureApt for more details
@@ -16,7 +16,7 @@ apt-ftparchive contents $D/binary > $D/binary/Contents
 
 # merge the result
 pushd $D/binary
-  mvn org.kohsuke:apt-ftparchive-merge:1.2:merge -Durl=$DEB_URL/binary/ -Dout=../merged
+  mvn org.kohsuke:apt-ftparchive-merge:1.4:merge -Durl=$DEB_URL/binary/ -Dout=../merged
 popd
 
 cat $D/merged/Packages > $D/binary/Packages
@@ -24,7 +24,7 @@ cat $D/merged/Packages | gzip -9c > $D/binary/Packages.gz
 cat $D/merged/Packages | bzip2 > $D/binary/Packages.bz2
 cat $D/merged/Packages | lzma > $D/binary/Packages.lzma
 cat $D/merged/Contents | gzip -9c > $D/binary/Contents.gz
-apt-ftparchive -c debian/release.conf release $D/binary > $D/binary/Release
+apt-ftparchive -c $bin/release.conf release $D/binary > $D/binary/Release
 # sign the release file
 rm $D/binary/Release.gpg || true
 gpg --no-use-agent --passphrase-file $GPG_PASSPHRASE_FILE -abs -o $D/binary/Release.gpg $D/binary/Release
