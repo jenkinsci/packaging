f10d5aa9b571af43c835d8ef71a32ec03178933d
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-04 00:09:28 -0700

    Improving tests

--- test/provision/suse.sh
+++ test/provision/suse.sh
@@ -2,9 +2,10 @@
 
 # get the IP address of the host of VirtualBox, and make it visible in expected name
 addr=$(netstat -rn | grep ^0.0.0.0 | cut -d " " -f10)
-host=$(echo @@RPM_URL@@ | tr '/' ':' | cut -d ':' -f4)
+host=$(echo @@SUSE_URL@@ | tr '/' ':' | cut -d ':' -f4)
 echo $addr $host | tee -a /etc/hosts > /dev/null
 
-sudo zypper addrepo @@SUSE_URL@@ @@ARTIFACTNAME@@
+zypper addrepo --gpg-auto-import-keys @@SUSE_URL@@ @@ARTIFACTNAME@@
+zypper install -y @@ARTIFACTNAME@@
 
 systemctl start @@ARTIFACTNAME@@
