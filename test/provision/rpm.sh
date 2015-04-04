a0abbd4e61f5baf82135866e168022d9b0e3d1af
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-04 10:08:53 -0700

    Extracted shared parts

--- test/provision/rpm.sh
+++ test/provision/rpm.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -ex
-
-# get the IP address of the host of VirtualBox, and make it visible in expected name
-addr=$(netstat -rn | grep ^0.0.0.0 | cut -d " " -f10)
-host=$(echo @@RPM_URL@@ | tr '/' ':' | cut -d ':' -f4)
-echo $addr $host | tee -a /etc/hosts > /dev/null
+/vagrant/provision/generated/common.sh
 
 wget -q -O /etc/yum.repos.d/jenkins.repo @@RPM_URL@@/@@ARTIFACTNAME@@.repo
 rpm --import @@RPM_URL@@/jenkins-ci.org.key
