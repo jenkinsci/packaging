9cd58db28ee2cc4f6bd9117268edfe0a27f249e0
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-03 17:50:09 -0700

    Removed unused variables

--- osx/build-on-jenkins.sh
+++ osx/build-on-jenkins.sh
@@ -5,10 +5,9 @@ D=/tmp/$$
 mkdir $D
 
 tar cvzf $D/script.tgz -C "$bin" .
-v=$(unzip -p "$2" META-INF/MANIFEST.MF | grep Implementation-Version | cut -d ' ' -f2 | tr -d '\r' | sed -e "s/-SNAPSHOT//" | sed -e "s/-beta-.*//")
-buildsh="$1/osx/build.sh"
-[ -e "$buildsh" ] || buildsh="$(dirname $0)/build.sh"
+
 java -jar $TARGET/jenkins-cli.jar dist-fork -z $D/script.tgz \
   -f binary/${ARTIFACTNAME}.war="${WAR}" \
-  -f build.sh=$bin/build.sh -l osx -F "${OSX}=jenkins.pkg" /bin/bash -ex build.sh binary/${ARTIFACTNAME}.war $VERSION $ARTIFACTNAME "$PRODUCTNAME"
-rm -rf /tmp/$$
+  -f build.sh=$bin/build.sh -l osx -F "${OSX}=jenkins.pkg" \
+  /bin/bash -ex build.sh binary/${ARTIFACTNAME}.war $VERSION $ARTIFACTNAME "$PRODUCTNAME"
+rm -rf $D
