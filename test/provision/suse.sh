af1f119644f7fc013bf105b03268e5e4db6691a5
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-03 23:39:30 -0700

    Added suse installation script

---
+++ test/provision/suse.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+# get the IP address of the host of VirtualBox, and make it visible in expected name
+addr=$(netstat -rn | grep ^0.0.0.0 | cut -d " " -f10)
+host=$(echo @@RPM_URL@@ | tr '/' ':' | cut -d ':' -f4)
+echo $addr $host | tee -a /etc/hosts > /dev/null
+
+sudo zypper addrepo @@SUSE_URL@@ @@ARTIFACTNAME@@
+
+systemctl start @@ARTIFACTNAME@@
