edb342dd529ee18e11fbb63ad276a4453b08021f
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-04 00:19:30 -0700

    automated installation

--- test/provision/suse.sh
+++ test/provision/suse.sh
@@ -5,7 +5,7 @@ addr=$(netstat -rn | grep ^0.0.0.0 | cut -d " " -f10)
 host=$(echo @@SUSE_URL@@ | tr '/' ':' | cut -d ':' -f4)
 echo $addr $host | tee -a /etc/hosts > /dev/null
 
-zypper addrepo --gpg-auto-import-keys @@SUSE_URL@@ @@ARTIFACTNAME@@
-zypper install -y @@ARTIFACTNAME@@
+zypper addrepo @@SUSE_URL@@ @@ARTIFACTNAME@@
+zypper --gpg-auto-import-keys install -y @@ARTIFACTNAME@@
 
 systemctl start @@ARTIFACTNAME@@
