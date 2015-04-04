7f27be97ef7af270d77eb659ded448aa823d2f5e
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-03 19:02:51 -0700

    More branding

--- osx/build.sh
+++ osx/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+WAR="$1"
+VERSION="$2"
+ARTIFACTNAME="${3:-jenkins}"
+PRODUCTNAME="${4:-Jenkins}"
+
 # Usage
-if [ -z "$1" ]; then
+if [ -z "$WAR" ]; then
 	echo "Usage: build.sh path/to/jenkins.war"
 	exit 1
 fi
@@ -19,29 +24,26 @@ fi
 PACKAGEMAKER="${PACKAGEMAKER_APP}/Contents/MacOS/PackageMaker"
 
 # Get the Jenkins version number
-cp "$1" $(dirname $0)/jenkins.war.tmp
-if [ -z "$2" ]; then
-  version=$(unzip -p $(dirname $0)/jenkins.war.tmp META-INF/MANIFEST.MF | grep Implementation-Version | cut -d ' ' -f2 | tr -d '\r' | sed -e "s/-SNAPSHOT//" | tr - . )
-else
-  version="$2"
+cp "$WAR" $(dirname $0)/jenkins.war.tmp
+if [ -z "$VERSION" ]; then
+  VERSION=$(unzip -p $(dirname $0)/jenkins.war.tmp META-INF/MANIFEST.MF | grep Implementation-Version | cut -d ' ' -f2 | tr -d '\r' | sed -e "s/-SNAPSHOT//" | tr - . )
 fi
-echo Version is $version
-PKG_NAME="jenkins.pkg"
-PKG_TITLE="Jenkins ${version}"
+echo Version is $VERSION
+PKG_NAME="${ARTIFACTNAME}.pkg"
+PKG_TITLE="${PRODUCTNAME} ${VERSION}"
 rm $(dirname $0)/jenkins.war.tmp
 
 # Fiddle with the package document so it points to the jenkins.war file provided
 PACKAGEMAKER_DOC="$(dirname $0)/JenkinsInstaller.pmdoc"
 mv $PACKAGEMAKER_DOC/01jenkins-contents.xml $PACKAGEMAKER_DOC/01jenkins-contents.xml.orig
-sed s,"pt=\".*\" m=","pt=\"${1}\" m=",g $PACKAGEMAKER_DOC/01jenkins-contents.xml.orig > $PACKAGEMAKER_DOC/01jenkins-contents.xml
+sed s,"pt=\".*\" m=","pt=\"${WAR}\" m=",g $PACKAGEMAKER_DOC/01jenkins-contents.xml.orig > $PACKAGEMAKER_DOC/01jenkins-contents.xml
 mv $PACKAGEMAKER_DOC/01jenkins.xml $PACKAGEMAKER_DOC/01jenkins.xml.orig
-sed s,"<installFrom mod=\"true\">.*</installFrom>","<installFrom mod=\"true\">${1}</installFrom>",g $PACKAGEMAKER_DOC/01jenkins.xml.orig > $PACKAGEMAKER_DOC/01jenkins.xml
-
+sed s,"<installFrom mod=\"true\">.*</installFrom>","<installFrom mod=\"true\">${WAR}</installFrom>",g $PACKAGEMAKER_DOC/01jenkins.xml.orig > $PACKAGEMAKER_DOC/01jenkins.xml
 # Build the package
 "${PACKAGEMAKER}" \
 	--doc "${PACKAGEMAKER_DOC}" \
 	--out "${PKG_NAME}" \
-	--version "${version}" \
+	--version "${VERSION}" \
 	--title "${PKG_TITLE}"
 
 # Reset the fiddling so git doesn't get confused
