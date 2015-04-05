30ad01c2781931c53c5317b7e8754b7c04d640f7
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-05 15:45:36 -0700

    Fixed up the way gpg is invoked.
    ... to use our own key/secret, and not one in the default keyring

--- deb/publish/publish.sh
+++ deb/publish/publish.sh
@@ -29,7 +29,8 @@ cat $D/merged/Contents | gzip -9c > $D/binary/Contents.gz
 apt-ftparchive -c $bin/release.conf release $D/binary > $D/binary/Release
 # sign the release file
 rm $D/binary/Release.gpg || true
-gpg --no-default-keyring --keyring $GPG_KEYRING --no-use-agent --passphrase-file $GPG_PASSPHRASE_FILE -abs -o $D/binary/Release.gpg $D/binary/Release
+gpg --batch --no-use-agent --no-default-keyring --keyring $GPG_KEYRING --secret-keyring=$GPG_SECRET_KEYRING --passphrase-file $GPG_PASSPHRASE_FILE \
+  -abs -o $D/binary/Release.gpg $D/binary/Release
 
 # generate web index
 $bin/gen.rb > $D/contents/index.html
