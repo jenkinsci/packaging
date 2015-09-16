1781ea90388fe19468707b58b249a4228a7209b5
Sam Van Oort <samvanoort@gmail.com>
2015-09-16 09:59:55 -0400

    Fixes and enhancements to the brand templating python script
    Clarify in-place file templating
    Better clean up text lines in brand variable lists
    Change comment format in brand variable list to a pound sign
    Better fail fast on un-set branding var (strip empty vals)

--- bin/branding.py
+++ bin/branding.py
@@ -6,20 +6,27 @@ import fnmatch
 import sys
 from optparse import OptionParser
 
-path = os.path.dirname(os.path.realpath(__file__))
+mypath = os.path.dirname(os.path.realpath(__file__))
 branding_values = dict()
+
+def cleaned_text_lines(textcontent):
+	""" Splits file content by line boundaries, strips leading/trailing whitespace, 
+	    and removes comment and whitespace lines """
+	lines = map(lambda x: x.strip(), textcontent.splitlines())
+	lines = filter(lambda x: x and not x.startswith('#'), lines)
+	return lines
 
 # Read branding environment variable list from branding file
 # For each variable read the corresponding env variable and store to branding map
-with open(os.path.join(path, 'branding.list'), 'r') as branding:
-	lines = filter(lambda x: x and not x.startswith('--'), branding.read().splitlines())
+with open(os.path.join(mypath, 'branding.list'), 'r') as branding:
+	lines = cleaned_text_lines(branding.read())
 	branding_values = {x:os.environ.get(x) for x in lines}
 
 # Read list of environment variables from file, where each is a path to a file
 # And for each read the content of that file to the branding map
 # brand-file contents from list of file name env variables and store to branding map as well
-with open(os.path.join(path, 'branding-files.list'), 'r') as branding:
-	lines = filter(lambda x: x and not x.startswith('--'), branding.read().splitlines())
+with open(os.path.join(mypath, 'branding-files.list'), 'r') as branding:
+	lines = cleaned_text_lines(branding.read())
 	for line in lines:
 		try:
 			# Read path for this line from environment var, and open that file
@@ -30,15 +37,18 @@ with open(os.path.join(path, 'branding-files.list'), 'r') as branding:
 		except Exception as e:
 			branding_values[line] = None
 
-# Do search and replace using string templating for a each file
+# Remove branding values with nothing set so we fail early if they are used in a template
+branding_values = dict(filter (lambda x: x[1], branding_values.items()))
+
+# Do IN-PLACE search and replace using string templating for a each file
 # Throws Exceptions if I/O fails or substitution is missing vars (safety check)
 def apply_templating_to_file(path, branding_map):
 	f = open(path, "r+")
 	f_content = string.Template(f.read()).substitute(branding_map)
 	f.seek(0)
 	f.write(f_content)
-	f.truncate()
-	del f
+	f.truncate()  # This removes any original content beyond the end of templated content
+	f.close()
 
 if (len(sys.argv) != 2):
 	raise Exception("Usage: branding.py [file or folder name]")
