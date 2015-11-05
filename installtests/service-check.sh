f9a26a0b16e329be84f3ca2d95dd6c3fe9382ce3
Sam Van Oort <samvanoort@gmail.com>
2015-11-05 15:50:35 -0500

    Refactor service functionality check

--- installtests/service-check.sh
+++ installtests/service-check.sh
@@ -1,115 +1,80 @@
 #!/bin/bash
 # Verify that service correctly handles starting and stopping process
+set -o nounset
 
-#seconds to wait for service response
-SERVICE_WAIT=2  
+#seconds to wait for service operation to finish
+SERVICE_WAIT=2
 
+# Report test results, by looking at status code same as expected
+# Arg 1: test name text
+# Arg 2: test status code output
+# Arg 3: test expected status code to pass
+# Arg 4: command output variable
+function report_test {
+    if [ "$2" -ne "$3" ]; then 
+        echo "TEST $1 FAILED with status code $2, expected $3"
+        echo "Test command output:"
+        echo "$4"
+    else
+        echo "TEST $1 PASSED with expected status code $2"
+    fi
+}
 
 # Check if jenkins user is present
 getent passwd jenkins
 USER_TEST=$?
-if [ $USER_TEST -ne 0 ]; then 
-    echo "User jenkins not installed"
-    exit 1
-fi
+report_test "Verify jenkins user created" $USER_TEST 0 $USER_TEST
 
 # Start service & capture standard error and exit code for service command
 SERVICE_OUTPUT=$(service jenkins start 2>&1)
 SERVICE_EXIT_CODE=$?
-
-if [ $SERVICE_EXIT_CODE -ne 0 ]; then
-    echo "Jenkins failed initial start with status code $SERVICE_EXIT_CODE"
-    echo "Message follows"
-    echo $SERVICE_OUTPUT
-    exit 1
-fi
-sleep $SERVICE_WAIT
+report_test "Jenkins initial service start" $SERVICE_EXIT_CODE 0 $SERVICE_OUTPUT
 
 # Test that Jenkins is actually working... by doing a basic CurL call
 CURL_OUTPUT=$(curl -sS 127.0.0.1:8080 -o /dev/null 2>&1)
 CURL_EXIT_CODE=$?
-if [ $CURL_EXIT_CODE -ne 0 ]; then
-    echo "Curl to jenkins host failed with code $CURL_EXIT_CODE"
-    echo "Curl error follows"
-    echo $CURL_OUTPUT
-    exit 1
-fi
+report_test "Curl to jenkins host" $CURL_EXIT_CODE 0 $CURL_OUTPUT
 
 # Check that jenkins status is correctly returned
 SERVICE_OUTPUT=$(service jenkins status 2>&1)
 SERVICE_EXIT_CODE=$?
-if [ $SERVICE_EXIT_CODE -ne 0 ]; then
-    echo "Jenkins inital start succeeded but status check failed with status code $SERVICE_EXIT_CODE"
-    echo "Message follows"
-    echo $SERVICE_OUTPUT
-    exit 1
-fi
+report_test "Jenkins service status after initial start" $SERVICE_EXIT_CODE 0 $SERVICE_OUTPUT
 
 # Check that jenkins restarts correctly
 SERVICE_OUTPUT=$(service jenkins restart 2>&1)
 SERVICE_EXIT_CODE=$?
-if [ $SERVICE_EXIT_CODE -ne 0 ]; then
-    echo "Jenkins first restart failed with exit code $SERVICE_EXIT_CODE"
-    echo "Message follows"
-    echo $SERVICE_OUTPUT
-    exit 1
-fi
+report_test "Jenkins service first restart" $SERVICE_EXIT_CODE 0 $SERVICE_OUTPUT
 
 sleep $SERVICE_WAIT
 
 # Check that jenkins stops cleanly
 SERVICE_OUTPUT=$(service jenkins stop 2>&1)
 SERVICE_EXIT_CODE=$?
-if [ $SERVICE_EXIT_CODE -ne 0 ]; then
-    echo "Jenkins stop failed with exit code $SERVICE_EXIT_CODE"
-    echo "Message follows"
-    echo $SERVICE_OUTPUT
-    exit 1
-fi
+report_test "Jenkins service stop" $SERVICE_EXIT_CODE 0 $SERVICE_OUTPUT
 
 sleep $SERVICE_WAIT
 
 # Check that jenkins status returns correctly after stop
 SERVICE_OUTPUT=$(service jenkins status 2>&1)
 SERVICE_EXIT_CODE=$?
-if [ $SERVICE_EXIT_CODE -ne 3 ]; then
-    echo "Jenkins status check after stop failed with exit code $SERVICE_EXIT_CODE"
-    echo "Message follows"
-    echo $SERVICE_OUTPUT
-    exit 1
-fi
+report_test "Jenkins service status check when stopped" $SERVICE_EXIT_CODE 3 $SERVICE_OUTPUT
 
 # Check that jenkins restarts correctly from stopped state
 SERVICE_OUTPUT=$(service jenkins restart 2>&1)
 SERVICE_EXIT_CODE=$?
-if [ $SERVICE_EXIT_CODE -ne 0 ]; then
-    echo "Jenkins first restart failed with exit code $SERVICE_EXIT_CODE"
-    echo "Message follows"
-    echo $SERVICE_OUTPUT
-    exit 1
-fi
+report_test "Jenkins service restart from stopped state" $SERVICE_EXIT_CODE 0 $SERVICE_OUTPUT
 
 sleep $SERVICE_WAIT
 
 # Check that jenkins status returns correctly after restart from stop
 SERVICE_OUTPUT=$(service jenkins status 2>&1)
 SERVICE_EXIT_CODE=$?
-if [ $SERVICE_EXIT_CODE -ne 0 ]; then
-    echo "Jenkins status check after stop failed with exit code $SERVICE_EXIT_CODE"
-    echo "Message follows"
-    echo $SERVICE_OUTPUT
-    exit 1
-fi
+report_test "Jenkins service status after restart from stopped state" $SERVICE_EXIT_CODE 0 $SERVICE_OUTPUT
 
 # Test that Jenkins is actually working after restart... by doing a basic CurL call
 CURL_OUTPUT=$(curl -sS 127.0.0.1:8080 -o /dev/null 2>&1)
 CURL_EXIT_CODE=$?
-if [ $CURL_EXIT_CODE -ne 0 ]; then
-    echo "Curl to jenkins host AFTER RESTART failed with code $CURL_EXIT_CODE"
-    echo "Curl error follows"
-    echo $CURL_OUTPUT
-    exit 1
-fi
+report_test "Curl to jenkins host AFTER restart from stopped" $CURL_EXIT_CODE 0 $CURL_OUTPUT
 
 
 ## TODO break jenkins then try it
