c864473fce36b5d1d928ddc443a7f9907c61e32c
Sam Van Oort <samvanoort@gmail.com>
2015-11-13 17:21:45 -0500

    Make all installer scripts executable mode

--- installtests/suse.sh
+++ installtests/suse.sh
@@ -4,11 +4,11 @@ set -e  # Exit on any command failure
 # Assume packaging is mounted to /tmp/packaging and built
 
 if [ -z "$1" ]; then
-  PKG_FOLDER='/tmp/packaging/target/suse/*.deb'
+  PKG_FOLDER='/tmp/packaging/target/suse/*.rpm'
 else 
   PKG_FOLDER="$1"
 fi
 
 # Ignore signature verification and default to yes
 zypper --non-interactive in curl
-zypper --no-gpg-checks --non-interactive in "$PKG_FOLDER"
+zypper --no-gpg-checks --non-interactive in $PKG_FOLDER
