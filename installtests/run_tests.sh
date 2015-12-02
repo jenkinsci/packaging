d46cda4b61ca248476edef4ee1ba882a9376a0c0
Sam Van Oort <samvanoort@gmail.com>
2015-12-01 19:40:05 -0500

    Fix opensuse test docker images by adding chkconfig

--- installtests/run_tests.sh
+++ installtests/run_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 PACKAGING_DIR="/Users/svanoort/Documents/packaging-2/packaging"
-docker run --rm -v "$PACKAGING_DIR":/tmp/packaging debian:wheezy /bin/bash /tmp/packaging/installtests/debian.sh && /tmp/packaging/installtests/service-check.sh
-docker run --rm -v "$PACKAGING_DIR":/tmp/packaging centos:7 /bin/bash /tmp/packaging/installtests/centos.sh && /tmp/packaging/installtests/service-check.sh
-docker run --rm -v "$PACKAGING_DIR":/tmp/packaging centos:6 /bin/bash /tmp/packaging/installtests/centos.sh && /tmp/packaging/installtests/service-check.sh
+docker run --rm -v "$PACKAGING_DIR":/tmp/packaging sudo-debian:wheezy /bin/bash /tmp/packaging/installtests/debian.sh && /tmp/packaging/installtests/service-check.sh
+docker run --rm -v "$PACKAGING_DIR":/tmp/packaging sudo-centos:7 /bin/bash /tmp/packaging/installtests/centos.sh && /tmp/packaging/installtests/service-check.sh
+docker run --rm -v "$PACKAGING_DIR":/tmp/packaging sudo-centos:6 /bin/bash /tmp/packaging/installtests/centos.sh && /tmp/packaging/installtests/service-check.sh
 
-docker run --rm -v "$PACKAGING_DIR":/tmp/packaging opensuse:13.2 /bin/bash /tmp/packaging/installtests/suse.sh && /tmp/packaging/installtests/service-check.sh
+docker run --rm -v "$PACKAGING_DIR":/tmp/packaging sudo-opensuse:13.2 /bin/bash /tmp/packaging/installtests/suse.sh && /tmp/packaging/installtests/service-check.sh
