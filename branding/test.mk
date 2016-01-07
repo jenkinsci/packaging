dcf858f4272be732dd1d245c0a4632497564f5ed
James Nord <jnord+github@cloudbees.com>
2016-01-07 19:23:12 +0000

    Add missing variables to test branding

--- branding/test.mk
+++ branding/test.mk
@@ -12,3 +12,13 @@ export PORT=7777
 
 export MSI_PRODUCTCODE=e76baa9f-2bb2-49e5-b518-8a5b7d1cd084
 export OSX_IDPREFIX=org.jenkins-ci.test
+export AUTHOR=Bogus user <bogus@example.org>
+export LICENSE=MIT/X License, GPL/CDDL, ASL2
+export HOMEPAGE=http://test.jenkins-ci.org/
+export CHANGELOG_PAGE=http://test.jenkins-ci.org/changelog
+
+# figure out the directory of this file
+BRANDING_DIR:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+
+export DESCRIPTION_FILE=$(BRANDING_DIR)/description-file
+export LICENSE_FILE=$(BRANDING_DIR)/license-mit
