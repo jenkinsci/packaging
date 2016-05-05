890e55961cbf30e7bb85e25ff2478fc8d6b95526
Sam Van Oort <samvanoort@gmail.com>
2016-05-05 10:30:42 -0400

    Docker packaging environment, add script to dynamically create a user for use within the container in mount + helper to assist with invocation

---
+++ docker/build-within-helper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Runs the packaging container as the local user, mounting the local folder to /localmount and setting it as workspace
+# Also creates the user and does the needful for them
+
+if [ -z "$1" ]; then 
+    echo "Usage: bash build-within-helper.sh $command"
+else
+    docker run --rm -v "$(pwd)":/localmount -w /localmount jenkins-packaging-builder:0.2 as_user.sh $(id -un) $(id -u) $(id -gn) $(id -g) "$1"
+fi
