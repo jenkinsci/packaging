2ab413be28a67b15413e65116c719c971fa8f145
Daniel Beck <daniel-beck@users.noreply.github.com>
2016-10-02 13:49:47 +0200

    Revert "[JENKINS-38134] Generate shasum for war, windows and osx installers"
    This reverts commit 916f556aad4d6fbbc7c8044c3056005cb29f652f.

--- msi/publish.sh
+++ msi/publish.sh
@@ -1,4 +1,2 @@
 #!/bin/bash -ex
-sha256sum ${MSI} > ${MSI_SHASUM}
-cat ${MSI_SHASUM}
-rsync -avz -e "ssh $SSH_OPTS" "$MSI" "$MSI_SHASUM" "$PKGSERVER:$MSIDIR/"
+rsync -avz -e "ssh $SSH_OPTS" "$MSI" "$PKGSERVER:$MSIDIR/"
