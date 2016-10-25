b68659325f630ce7c454cb50e4f530f9b0123fd3
Armando Fernandez <armfergom@gmail.com>
2016-10-25 12:24:25 +0200

    [JENKINS-38134] Enabled pipefail in modified scripts

--- msi/publish.sh
+++ msi/publish.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -ex -o pipefail
 sha256sum ${MSI} | sed 's, .*/, ,' > ${MSI_SHASUM}
 cat ${MSI_SHASUM}
 rsync -avz -e "ssh $SSH_OPTS" "$MSI" "$MSI_SHASUM" "$PKGSERVER:$MSIDIR/"
--- osx/publish.sh
+++ osx/publish.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -ex -o pipefail
 sha256sum ${OSX} | sed 's, .*/, ,' > ${OSX_SHASUM}
 cat ${OSX_SHASUM}
 rsync -avz -e "ssh $SSH_OPTS" "${OSX}" "${OSX_SHASUM}" "$PKGSERVER:$OSXDIR/"
