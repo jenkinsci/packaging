29dc484be101556c24e03c68145a867964c10346
Oleg Nenashev <o.v.nenashev@gmail.com>
2017-03-08 12:32:50 +0100

    Get rid of "Continuous Integration Server" in the docs

--- branding/test.mk
+++ branding/test.mk
@@ -7,7 +7,7 @@ export PRODUCTNAME=Jenkins Test
 export ARTIFACTNAME=jenkinstest
 export CAMELARTIFACTNAME=JenkinsTest
 export VENDOR=Jenkins Test project
-export SUMMARY=Jenkins Continuous Integration Server (Test)
+export SUMMARY=Jenkins Automation Server (Test)
 export PORT=7777
 
 export MSI_PRODUCTCODE=e76baa9f-2bb2-49e5-b518-8a5b7d1cd084
