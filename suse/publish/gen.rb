d3d782d23efec26ba9e48f9c13c9bc51eac59a6f
Sam Van Oort <samvanoort@gmail.com>
2017-05-05 15:10:09 -0400

    Note the official Java versions required for packages

--- suse/publish/gen.rb
+++ suse/publish/gen.rb
@@ -25,7 +25,12 @@ sudo zypper addrepo -f #{url}/ #{artifactName}
 
 <p>
 You will need to explicitly install a Java runtime environment, because Oracle's Java RPMs are incorrect and fail to register as providing a java dependency. 
-Thus, adding an explicit dependency requirement on Java would have forced installation of the OpenJDK JVM.
+Thus, adding an explicit dependency requirement on Java would force installation of the OpenJDK JVM.
+
+<ul>
+  <li>2.54 (2017-04) and newer: Java 8</li>
+  <li>1.612 (2015-05) and newer: Java 7</li>
+</ul>
 
 <p>
 With that set up, the #{productName} package can be installed with <tt>zypper install #{artifactName}</tt>
