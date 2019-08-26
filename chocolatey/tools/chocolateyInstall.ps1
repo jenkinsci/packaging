20b99a8372523917d776c95182e1bbd978ced0ab
Alex Earl <slide.o.mix@gmail.com>
2019-08-26 12:34:06 -0700

    Update Windows installer and add Chocolatey package

---
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+. "$toolsDir\helpers.ps1"
+
+$pp = Get-PackageParameters
+
+$arguments = @{
+    packageName = $env:chocolateyPackageName
+    file        = "$toolsDir\jenkins.msi"
+    port        = if ($pp.Port) { $pp.Port } else { 8080 }
+    serviceName = "Jenkins"
+    destination = ""
+    toolsDir    = $toolsDir
+}
+
+if (-not (Assert-TcpPortIsOpen $arguments.port)) {
+    throw 'Please specify a different port number...'
+}
+
+Install-Jenkins $arguments
