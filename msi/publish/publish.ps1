2da9ca46241a43e1bca5004ff67f8a3ca95a5fd5
Olblak <me@olblak.com>
2019-11-20 15:35:25 +0100

    msi publish script source BUILDENV

--- msi/publish/publish.ps1
+++ msi/publish/publish.ps1
@@ -1,3 +1,8 @@
+Get-Content .\$env:BUILDENV | Select-String -Match "^export" -Replace "export ",""| ForEach-Object {
+    $array= $_[0].ToString().split("=")
+    [System.Environment]::SetEnvironmentVariable($array[0], $array[1])
+}
+
 Write-Host "Copying binaries to $($env:MSIDIR)"
 if(-not Test-Path $env:MSIDIR) {
   New-Item -ItemType Directory -Path $env:MSIDIR
