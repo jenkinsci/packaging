66aa223e9e325762e273a373ef5ec3d0144f6a52
Olblak <me@olblak.com>
2020-02-25 14:06:21 +0100

    Concat MSDIR and RELEASELINE in the msi publishing script

--- msi/publish/publish.ps1
+++ msi/publish/publish.ps1
@@ -8,12 +8,15 @@ Get-Content .\$env:BUILDENV | Select-String -Pattern "^export"| ForEach-Object {
     [System.Environment]::SetEnvironmentVariable($array[0].Replace("export ",""), $array[1])
 }
 
-Write-Host "Copying binaries to $($env:MSIDIR)"
+# I couldn't find how interpolate MSIDIR="/packages/binary/windows$($env:RELEASELINE)"
+Set-Variable -Name MSIDIR -Value "$env:MSIDIR$env:RELEASELINE"
 
-if(!(Test-Path $env:MSIDIR)) {
-  New-Item -ItemType Directory -Path $env:MSIDIR
+Write-Host "Copying binaries to $MSIDIR"
+
+if(!(Test-Path $MSIDIR)) {
+  New-Item -ItemType Directory -Path $MSIDIR
 }
 
-Get-ChildItem -Path '.\msi\build\bin\Release\en-US\*' -File -Include *.msi,*.msi.sha256 | Copy-Item -Destination $env:MSIDIR
+Get-ChildItem -Path '.\msi\build\bin\Release\en-US\*' -File -Include *.msi,*.msi.sha256 | Copy-Item -Destination $MSIDIR
 
-Get-ChildItem -Path $env:MSIDIR
+Get-ChildItem -Path $MSIDIR
