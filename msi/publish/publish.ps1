93cc1a751b62e83022b209cd262bc8103532c824
Olblak <me@olblak.com>
2020-02-27 10:56:35 +0100

    Refactor MSI publishing script

--- msi/publish/publish.ps1
+++ msi/publish/publish.ps1
@@ -1,22 +1,36 @@
-Get-Content .\$env:BRAND | Select-String -Pattern "^export"| ForEach-Object {
-    $array= $_[0].ToString().split("=")
-    [System.Environment]::SetEnvironmentVariable($array[0].Replace("export ",""), $array[1])
+function Export-Variables($file) {
+    Get-Content $file | Select-String -Pattern "^export"| ForEach-Object {
+        $array= $_[0].ToString().split("=")
+        $array[0] = $array[0].Replace("export","").Trim()
+        if($array[1].Contains('${')) {
+            $array[1] = $ExecutionContext.InvokeCommand.ExpandString($array[1].Replace('${', '${env:'))
+        } elseif($array[1] -match '\$\(([^)]*)\)(.*)') {
+            $command = $Matches[1].Trim()
+            $rest = $Matches[2]
+            if($command.StartsWith('realpath')) {
+                $command = Invoke-Expression -Command $command.Replace('realpath', 'Resolve-Path')
+            } else {
+                Write-Error "Unknown command to convert: $command"
+            }
+            $array[1] = '{0}{1}' -f $command,$rest
+            $array[1] = $ExecutionContext.InvokeCommand.ExpandString($array[1])
+        }
+        [System.Environment]::SetEnvironmentVariable($array[0], $array[1])
+    }
 }
 
-Get-Content .\$env:BUILDENV | Select-String -Pattern "^export"| ForEach-Object {
-    $array= $_[0].ToString().split("=")
-    [System.Environment]::SetEnvironmentVariable($array[0].Replace("export ",""), $array[1])
+if(($null -ne $env:BRAND) -and (Test-Path $env:BRAND)) {
+    Export-Variables .\$env:BRAND
 }
 
-# I couldn't find how interpolate MSIDIR="/packages/binary/windows$($env:RELEASELINE)"
-Set-Variable -Name MSIDIR -Value "$env:MSIDIR$env:RELEASELINE"
+Export-Variables .\$env:BUILDENV
 
-Write-Host "Copying binaries to $MSIDIR"
+Write-Host "Copying binaries to ${env:MSIDIR}"
 
-if(!(Test-Path $MSIDIR)) {
-  New-Item -ItemType Directory -Path $MSIDIR
+if(!(Test-Path $env:MSIDIR)) {
+  New-Item -ItemType Directory -Path $env:MSIDIR
 }
 
-Get-ChildItem -Path '.\msi\build\bin\Release\en-US\*' -File -Include *.msi,*.msi.sha256 | Copy-Item -Destination $MSIDIR
+Get-ChildItem -Path '.\msi\build\bin\Release\en-US\*' -File -Include *.msi,*.msi.sha256 | Copy-Item -Destination $env:MSIDIR
 
-Get-ChildItem -Path $MSIDIR
+Get-ChildItem -Path $env:MSIDIR
