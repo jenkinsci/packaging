d604f127d78a1125c8cda2cd0ebd72c608efbff0
Mark Waite <mark.earl.waite@gmail.com>
2020-04-03 17:55:04 -0600

    Change to /tmp/packaging in centos test

--- installtests/centos.sh
+++ installtests/centos.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 set -x  # Exit on any command failure or unset variables, can't use -u because of line 6
 
-. "$(dirname $0)/sh2ju.sh"
-
 if [ -z "$1" ]; then
   PKG_FOLDER=$(ls /tmp/packaging/target/rpm/*.rpm)
-else 
+  # Change to /tmp/packaging so that results are written in /tmp/packaging/results
+  cd /tmp/packaging
+else
   PKG_FOLDER="$1"
 fi
 
-dockerInstall() {
-    # Assume packaging is mounted to /tmp/packaging and built
-    yum install -y curl
-    yum install -y system-config-services java-1.8.0-openjdk  # First is b/c docker does not include service command, second is prereq
-    yum -y --nogpgcheck localinstall "$PKG_FOLDER"
+. "$(dirname $0)/sh2ju.sh"
+
+install_failure_message="yum install command failed on $PKG_FOLDER"
+
+centos_docker_install() {
+    # Tests need the curl command
+    # Docker image does not include the service command
+    # Tests need the yum-verify command
+    yum install -y curl system-config-services yum-verify # docker does not include service command
+    # Assumes rpm file is available as $PKG_FOLDER
+    yum -y localinstall "$PKG_FOLDER" || echo "$install_failure_message"
 }
 
-juLog -name=centosDockerInstall dockerInstall
+juLog -error="$install_failure_message" -name=centosDockerInstall centos_docker_install
