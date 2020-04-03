0ce36dabcca1561c1d03a146a28768afacd88859
Mark Waite <mark.earl.waite@gmail.com>
2020-04-03 16:04:18 -0600

    Add dpkg audit and dpkg verify checks

--- installtests/debian.sh
+++ installtests/debian.sh
@@ -2,23 +2,78 @@
 set -x  # Exit on any command failure, can't use -u because of check for non-zero length of arg 1
 
 if [ -z "$1" ]; then
-  PKG_FOLDER=$(ls /tmp/packaging/target/debian/*.deb)
+  JENKINS_DEB_INSTALLER_FILE=$(ls /tmp/packaging/target/debian/*.deb)
   # Change to /tmp/packaging so that results are written in /tmp/packaging/results
   cd /tmp/packaging
 else
-  PKG_FOLDER="$1"
+  JENKINS_DEB_INSTALLER_FILE="$1"
 fi
 
 . "$(dirname $0)/sh2ju.sh"
 
-dockerInstall() {
+install_failure_message="dpkg install failed on $JENKINS_DEB_INSTALLER_FILE file"
+
+docker_dpkg_install() {
     # Installation within Docker
-    # Assume packaging is mounted to /tmp/packaging and built
+    # Update with latest list of available packages
     apt-get update
+
+    # Assume packaging is already built and is available in $JENKINS_DEB_INSTALLER_FILE
     # Below will fail because missing dependencies, which apt-get will install
-    dpkg -i "$PKG_FOLDER" || true
-    apt-get install -fy && apt-get install -fy curl
-    dpkg -i "$PKG_FOLDER"
+    echo
+    echo "===== Installing $JENKINS_DEB_INSTALLER_FILE without dependencies"
+    echo
+    dpkg --install "$JENKINS_DEB_INSTALLER_FILE" || true
+
+    # Install Jenkins package dependencies, then install curl while ignoring curl install output
+    # Output should detect issues with Jenkins dependencies, curl is a tool needed later
+    echo
+    echo "===== Resolving dependencies of $JENKINS_DEB_INSTALLER_FILE with apt-get"
+    echo
+    apt-get install -fy && apt-get install -fy curl > /dev/null 2>&1
+
+    # Remove jenkins installed by first call to dpkg
+    echo
+    echo "===== Removing flawed installation of $JENKINS_DEB_INSTALLER_FILE"
+    echo
+    dpkg --purge jenkins
+
+    # Install Jenkins from the packaging directory
+    echo
+    echo "===== Installing $JENKINS_DEB_INSTALLER_FILE with dependencies already installed"
+    echo
+    dpkg --install "$JENKINS_DEB_INSTALLER_FILE" || echo "$install_failure_message"
 }
 
-juLog -name=debianDockerInstall dockerInstall
+juLog -error="$install_failure_message" -name=debianDockerInstall docker_dpkg_install
+
+##
+# Use dpkg verify to check package contents
+##
+
+verify_failure_message="dpkg verify failed on jenkins package"
+
+docker_dpkg_verify() {
+    # Use dpkg verify to check contents of the jenkins package
+    echo
+    echo "===== Verifying jenkins package with dpkg"
+    echo
+    dpkg --verify jenkins || echo "$verify_failure_message"
+}
+
+juLog -error="$verify_failure_message" -name=debianDockerVerify docker_dpkg_verify
+
+##
+# Use dpkg audit to check package contents
+##
+
+audit_failure_message="dpkg audit failed on jenkins package"
+
+docker_dpkg_audit() {
+    echo
+    echo "===== Auditing jenkins package with dpkg"
+    echo
+    dpkg --audit jenkins  || echo "$audit_failure_message"
+}
+
+juLog -error="$audit_failure_message" -name=debianDockerAudit docker_dpkg_audit
