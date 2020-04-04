8715c38e6af5a4565f89f653d8d324543664f0e5
Mark Waite <mark.earl.waite@gmail.com>
2020-04-03 21:10:47 -0600

    Use ARG instead of modifying Dockerfile

--- docker/build-sudo-images.sh
+++ docker/build-sudo-images.sh
@@ -4,9 +4,7 @@
 # Note: this modifies the original Dockerfiles so the current user can sudo
 
 cd "$(dirname "$0")"
-sed -i.orig -e "s#@@MYUSERID@@#`id -u`#g" sudo-*/Dockerfile
-docker build -t sudo-debian:buster sudo-debian
-docker build -t sudo-centos:7 sudo-centos7
-docker build -t sudo-opensuse:15.1 sudo-opensuse
-docker build -t sudo-ubuntu:18.04 sudo-ubuntu18
-docker build -t sudo-ubuntu:19.10 sudo-ubuntu19
+docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-debian:buster sudo-debian
+# docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-opensuse:15.1 sudo-opensuse
+docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-ubuntu:18.04  sudo-ubuntu18
+docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-ubuntu:19.10  sudo-ubuntu19
