248704d500d3f48bccf4b5e093eae2f4176155bf
Mark Waite <mark.earl.waite@gmail.com>
2020-04-05 13:47:32 -0600

    Check OpenSUSE 15.1 install
    Expected to fail.  More dev needed

--- docker/build-sudo-images.sh
+++ docker/build-sudo-images.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-debian:stable sudo-debian
 docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-debian:oldstable sudo-debian-oldstable
 docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-debian:testing sudo-debian-testing
-# docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-opensuse:15.1 sudo-opensuse
+docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-opensuse:15.1 sudo-opensuse
 docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-ubuntu:16.04  sudo-ubuntu16
 docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-ubuntu:18.04  sudo-ubuntu18
 docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-ubuntu:19.10  sudo-ubuntu19
--- installtests/suse.sh
+++ installtests/suse.sh
@@ -12,11 +12,17 @@ fi
 
 . "$(dirname $0)/sh2ju.sh"
 
+# Read operating system information into variables
+
+. /etc/os-release
+
+OS="${ID}.${VERSION_ID}" # opensuse-leap.15.1 so that JUnit package naming can be used
+
 install_failure_message="zypper install failed on $JENKINS_SUSE_INSTALLER_FILE"
 
 suse_zypper_install() {
-    # Ignore signature verification and default to yes
-    zypper --no-gpg-checks --non-interactive in $JENKINS_SUSE_INSTALLER_FILE || echo "$install_failure_message"
+    # Ignore signature verification - OpenSUSE Jenkins 2.229 package is not GPG signed (why not?)
+    zypper --no-gpg-checks --non-interactive install insserv-compat $JENKINS_SUSE_INSTALLER_FILE || echo "$install_failure_message"
 }
 
-juLog -error="$JENKINS_SUSE_INSTALLER_FILE" -name=suseDockerInstall suse_zypper_install
+juLog -error="$JENKINS_SUSE_INSTALLER_FILE" -suite="${OS}.install" -name="DockerInstall" suse_zypper_install
