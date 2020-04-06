4a1a6821eb8c77877653c7ce656c180cd307a2ac
Mark Waite <mark.earl.waite@gmail.com>
2020-04-06 15:44:57 -0600

    Add OpenSUSE service check

--- installtests/run_tests.sh
+++ installtests/run_tests.sh
@@ -16,7 +16,7 @@ echo "Packaging directory is: $PACKAGING_DIR"
 docker run --rm -v "$PACKAGING_DIR":/tmp/packaging sudo-debian:oldstable /bin/bash /tmp/packaging/installtests/test_helper.sh /tmp/packaging/installtests/debian.sh /tmp/packaging/installtests/service-check.sh
 docker run --rm -v "$PACKAGING_DIR":/tmp/packaging sudo-debian:stable    /bin/bash /tmp/packaging/installtests/test_helper.sh /tmp/packaging/installtests/debian.sh /tmp/packaging/installtests/service-check.sh
 docker run --rm -v "$PACKAGING_DIR":/tmp/packaging sudo-debian:testing   /bin/bash /tmp/packaging/installtests/test_helper.sh /tmp/packaging/installtests/debian.sh /tmp/packaging/installtests/service-check.sh
-docker run --rm -v "$PACKAGING_DIR":/tmp/packaging sudo-opensuse:15.1    /bin/bash /tmp/packaging/installtests/test_helper.sh /tmp/packaging/installtests/suse.sh
+docker run --rm -v "$PACKAGING_DIR":/tmp/packaging sudo-opensuse:15.1    /bin/bash /tmp/packaging/installtests/test_helper.sh /tmp/packaging/installtests/suse.sh   /tmp/packaging/installtests/service-check.sh
 docker run --rm -v "$PACKAGING_DIR":/tmp/packaging sudo-ubuntu:16.04     /bin/bash /tmp/packaging/installtests/test_helper.sh /tmp/packaging/installtests/debian.sh /tmp/packaging/installtests/service-check.sh
 docker run --rm -v "$PACKAGING_DIR":/tmp/packaging sudo-ubuntu:18.04     /bin/bash /tmp/packaging/installtests/test_helper.sh /tmp/packaging/installtests/debian.sh /tmp/packaging/installtests/service-check.sh
 docker run --rm -v "$PACKAGING_DIR":/tmp/packaging sudo-ubuntu:19.10     /bin/bash /tmp/packaging/installtests/test_helper.sh /tmp/packaging/installtests/debian.sh /tmp/packaging/installtests/service-check.sh
