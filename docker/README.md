07c08f9d0e4468daf02546a3a05aebcc1671d957
Mark Waite <mark.earl.waite@gmail.com>
2020-05-05 15:25:41 -0600

    Build base docker images for install and service tests

--- docker/README.md
+++ docker/README.md
@@ -3,9 +3,9 @@
 This offers a simplified build environment for testing packaging
 
 ## The sudo containers ##
-* These images supply sudo access to enable installation tests (which require root or sudo) with the Jenkins docker-workflow plugin.  They use templating to add the current user as a sudoer.
+* These images supply sudo access to enable installation tests (which require root or sudo) with the Jenkins docker-workflow plugin.  They use an ARG to add the current user as a sudoer.
 * These **MUST** be built by the build-sudo-images.sh script before use in docker workflow
 * The script reads the local user ID, creates a 'mysudoer' user with sudo access in these containers with no-password sudo
 * For CentOS images, the sudoer requiretty option is turned off to allow-non-terminal use
 * For specific environments additional requirements for services may be added (example: initscripts for CentOS7, since service support is not baked in)
-* An added perk of these is that the debian-based ones update the apt database to save time on future installs
+* An added perk of these is that they install the default JDK for faster testing
--- docker/build-sudo-images.sh
+++ docker/build-sudo-images.sh
@@ -4,10 +4,14 @@
 # Note: this modifies the original Dockerfiles so the current user can sudo
 
 cd "$(dirname "$0")"
-sed -i .orig -e "s#@@MYUSERID@@#`id -u`#g" sudo-*/Dockerfile
-docker build -t sudo-debian:wheezy sudo-debian
-docker build -t sudo-centos:6 sudo-centos6
-docker build -t sudo-centos:7 sudo-centos7
-docker build -t sudo-opensuse:13.2 sudo-opensuse
-docker build -t sudo-ubuntu:14.04 sudo-ubuntu14
-docker build -t sudo-ubuntu:15.10 sudo-ubuntu15
+docker build                                      -t sudo-centos:7 sudo-centos7
+docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-debian:stable sudo-debian
+docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-debian:oldstable sudo-debian-oldstable
+docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-debian:testing sudo-debian-testing
+docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-opensuse:15.1 sudo-opensuse
+docker build                                      -t sudo-fedora:31 sudo-fedora31
+docker build                                      -t sudo-fedora:32 sudo-fedora32
+docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-ubuntu:16.04  sudo-ubuntu16
+docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-ubuntu:18.04  sudo-ubuntu18
+docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-ubuntu:19.10  sudo-ubuntu19
+docker build --build-arg USER_ID=$(id -u ${USER}) -t sudo-ubuntu:20.04  sudo-ubuntu20
