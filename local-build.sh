28b545aae47eafc6d155ea8c48e41fc3a0b44e4f
Basil Crow <me@basilcrow.com>
2022-01-15 12:41:02 -0800

    Better line breaks

--- local-build.sh
+++ local-build.sh
@@ -5,5 +5,7 @@ cd "$(dirname "$0")"
 ./prep.sh
 make war deb rpm suse
 
-# produces: target/debian/jenkins_${JENKINS_VERSION}_all.deb,target/rpm/jenkins-${JENKINS_VERSION}-1.1.noarch.rpm,target/suse/jenkins-${JENKINS_VERSION}-1.2.noarch.rpm
+# produces: target/debian/jenkins_${JENKINS_VERSION}_all.deb
+# produces: target/rpm/jenkins-${JENKINS_VERSION}-1.1.noarch.rpm
+# produces: target/suse/jenkins-${JENKINS_VERSION}-1.2.noarch.rpm
 exit 0
