79ae29cc8394e35c6b5f63b3084e5251ddf4b270
Basil Crow <me@basilcrow.com>
2022-01-17 08:09:18 -0800

    Add automated build (#228)

---
+++ prep.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -eux
+set -o pipefail
+cd "$(dirname "$0")"
+
+# TODO jenkins-infra/release performs similar preparatory actions: downloading
+# the WAR and importing the GPG key. A common interface for the preparatory
+# actions should be designed that meets the needs of both local testing and
+# releases, ideally implemented in the Makefile. Then both this repository and
+# jenkins-infra/release should be refactored to consume the new functionality.
+
+if [[ ! -f $WAR ]]; then
+	mkdir -p "$(dirname "${WAR}")"
+	jv download
+fi
+
+if ! gpg --fingerprint "${GPG_KEYNAME}"; then
+	gpg --import --batch "${GPG_KEYRING}" "${GPG_SECRET_KEYRING}"
+fi
+
+# produces: target/war/jenkins.war
+exit 0
