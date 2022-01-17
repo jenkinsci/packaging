ec0f0faf381fce8efcae25ad760d122260acfb52
Basil Crow <me@basilcrow.com>
2022-01-17 07:08:18 -0800

    Remove non-interactive mode

--- bldenv.sh
+++ bldenv.sh
@@ -16,45 +16,23 @@ GPG_KEYNAME=${GPG_KEYNAME:-test}
 GPG_KEYRING=${GPG_KEYRING:-${WORKSPACE}/credentials/${GPG_KEYNAME}.gpg}
 GPG_SECRET_KEYRING=${GPG_SECRET_KEYRING:-${WORKSPACE}/credentials/${GPG_KEYNAME}.secret.gpg}
 
-if [[ $# -eq 0 ]]; then
-	exec docker run \
-		-e "WORKSPACE=${WORKSPACE}" \
-		-e "JENKINS_VERSION=${JENKINS_VERSION}" \
-		-e "JENKINS_DOWNLOAD_URL=${JENKINS_DOWNLOAD_URL}" \
-		-e "WAR=${WAR}" \
-		-e "MSI=${MSI}" \
-		-e "BRAND=${BRAND}" \
-		-e "RELEASELINE=${RELEASELINE}" \
-		-e "ORGANIZATION=${ORGANIZATION}" \
-		-e "BUILDENV=${BUILDENV}" \
-		-e "CREDENTIAL=${CREDENTIAL}" \
-		-e "GPG_KEYNAME=${GPG_KEYNAME}" \
-		-e "GPG_KEYRING=${GPG_KEYRING}" \
-		-e "GPG_SECRET_KEYRING=${GPG_SECRET_KEYRING}" \
-		-it \
-		--rm \
-		-v "${PWD}:${WORKSPACE}" \
-		-w "${WORKSPACE}" \
-		jenkinsciinfra/packaging:latest \
-		/bin/bash
-else
-	exec docker run \
-		-e "WORKSPACE=${WORKSPACE}" \
-		-e "JENKINS_VERSION=${JENKINS_VERSION}" \
-		-e "JENKINS_DOWNLOAD_URL=${JENKINS_DOWNLOAD_URL}" \
-		-e "WAR=${WAR}" \
-		-e "MSI=${MSI}" \
-		-e "BRAND=${BRAND}" \
-		-e "RELEASELINE=${RELEASELINE}" \
-		-e "ORGANIZATION=${ORGANIZATION}" \
-		-e "BUILDENV=${BUILDENV}" \
-		-e "CREDENTIAL=${CREDENTIAL}" \
-		-e "GPG_KEYNAME=${GPG_KEYNAME}" \
-		-e "GPG_KEYRING=${GPG_KEYRING}" \
-		-e "GPG_SECRET_KEYRING=${GPG_SECRET_KEYRING}" \
-		--rm \
-		-v "${PWD}:${WORKSPACE}" \
-		-w "${WORKSPACE}" \
-		jenkinsciinfra/packaging:latest \
-		/bin/bash -c "$*"
-fi
+exec docker run \
+	-e "WORKSPACE=${WORKSPACE}" \
+	-e "JENKINS_VERSION=${JENKINS_VERSION}" \
+	-e "JENKINS_DOWNLOAD_URL=${JENKINS_DOWNLOAD_URL}" \
+	-e "WAR=${WAR}" \
+	-e "MSI=${MSI}" \
+	-e "BRAND=${BRAND}" \
+	-e "RELEASELINE=${RELEASELINE}" \
+	-e "ORGANIZATION=${ORGANIZATION}" \
+	-e "BUILDENV=${BUILDENV}" \
+	-e "CREDENTIAL=${CREDENTIAL}" \
+	-e "GPG_KEYNAME=${GPG_KEYNAME}" \
+	-e "GPG_KEYRING=${GPG_KEYRING}" \
+	-e "GPG_SECRET_KEYRING=${GPG_SECRET_KEYRING}" \
+	-it \
+	--rm \
+	-v "${PWD}:${WORKSPACE}" \
+	-w "${WORKSPACE}" \
+	jenkinsciinfra/packaging:latest \
+	/bin/bash
