8c668efc84d74dfc5e6f71f427bba773963be17e
Basil Crow <me@basilcrow.com>
2022-01-17 11:34:56 -0800

    Format with Black

--- bin/branding.py
+++ bin/branding.py
@@ -9,92 +9,113 @@ import sys
 # Usage is 'python branding.py {file or folder}'
 # Env variables to use are given in BRANDING_ENV_VARIABLE_LIST
 # Env variables giving *paths* to read content for templating are given in BRANDING_ENV_PATH_LIST
-# Author Sam Van Oort <samvanoort@gmail.com> 
+# Author Sam Van Oort <samvanoort@gmail.com>
 
-BRANDING_ENV_VARIABLE_LIST = 'branding.list'
-BRANDING_ENV_PATH_LIST = 'branding-files.list'
+BRANDING_ENV_VARIABLE_LIST = "branding.list"
+BRANDING_ENV_PATH_LIST = "branding-files.list"
+
 
 class CustomTemplate(string.Template):
-	delimiter = '@@'
-	pattern = r""" %(delim)s(?:
+    delimiter = "@@"
+    pattern = r""" %(delim)s(?:
       (?P<escaped>) |   # Nope, taken care of by the delimiter not being a valid id character
       (?P<named>%(id)s)      |   # delimiter and a Python identifier
       (?P<braced>%(id)s)   |   # we don't support braced identifiers, since it's surrounded by delimiters
       (?P<invalid>)            # no matches
-    )%(delim)s """ % dict(delim=re.escape('@@'), id=string.Template.idpattern)
+    )%(delim)s """ % dict(
+        delim=re.escape("@@"), id=string.Template.idpattern
+    )
+
 
 def clean_text_lines(textcontent):
-	""" Splits file content by line boundaries, strips leading/trailing whitespace, 
-	    and removes comments with # and -- prefixes and whitespace lines """
-	lines = map(lambda x: x.strip(), textcontent.splitlines())
-	lines = filter(lambda x: x and not x.startswith('#') and not x.startswith('--'), lines)
-	return lines
+    """Splits file content by line boundaries, strips leading/trailing whitespace,
+    and removes comments with # and -- prefixes and whitespace lines"""
+    lines = map(lambda x: x.strip(), textcontent.splitlines())
+    lines = filter(
+        lambda x: x and not x.startswith("#") and not x.startswith("--"), lines
+    )
+    return lines
+
 
 def read_env_variable_list(path):
-	""" Read a list of environment variables from a file (one per line) and get a dict of var:value """
-	with open(path, 'r') as env_file:
-		lines = clean_text_lines(env_file.read())
-		return {x: os.environ.get(x) for x in lines}
+    """Read a list of environment variables from a file (one per line) and get a dict of var:value"""
+    with open(path, "r") as env_file:
+        lines = clean_text_lines(env_file.read())
+        return {x: os.environ.get(x) for x in lines}
+
 
 def read_file_content(value_path_dictionary):
-	""" For a {variable_name:file_path} dictionary, read each file and 
-	    return a dictionary of {variable:file_content}
+    """For a {variable_name:file_path} dictionary, read each file and
+    return a dictionary of {variable:file_content}
 
-	    If paths are null/empty, they are not read to result. """
-	output = dict()
-	filtered = filter(lambda x: x[1] and x[1].strip(), value_path_dictionary.items())
-	for variable, path in filtered:
-		with open(path, 'r') as f:
-			output[variable] = f.read()
-	return output
+    If paths are null/empty, they are not read to result."""
+    output = {}
+    filtered = filter(lambda x: x[1] and x[1].strip(), value_path_dictionary.items())
+    for variable, path in filtered:
+        with open(path, "r") as f:
+            output[variable] = f.read()
+    return output
+
 
 def read_branding_variables(base_path, env_variables_list, file_variables_list):
-	""" Read branding variables from files/environment and return result """
+    """Read branding variables from files/environment and return result"""
 
-	raw_variables = read_env_variable_list(os.path.join(base_path, env_variables_list))
-	file_variables = read_env_variable_list(os.path.join(base_path, file_variables_list))
-	raw_variables.update(read_file_content(file_variables))  # Add file content variables
-	return raw_variables
+    raw_variables = read_env_variable_list(os.path.join(base_path, env_variables_list))
+    file_variables = read_env_variable_list(
+        os.path.join(base_path, file_variables_list)
+    )
+    raw_variables.update(
+        read_file_content(file_variables)
+    )  # Add file content variables
+    return raw_variables
+
 
 def apply_template(input_string, branding_map):
-	""" Applies templating in a back-compatible fashion """
-	return CustomTemplate(input_string).substitute(branding_map)
+    """Applies templating in a back-compatible fashion"""
+    return CustomTemplate(input_string).substitute(branding_map)
+
 
 def apply_templating_to_file(path, branding_map):
-	""" Do IN-PLACE search and replace using string templating for a each file
-		Throws Exceptions if I/O fails or substitution is missing vars (safety check)
-	"""
-	with open(path, "r+") as f:
-		f_content = apply_template(f.read(), branding_map)
-		f.seek(0)
-		f.write(f_content)
-		f.truncate()  # This removes any original content beyond the end of templated content
+    """Do IN-PLACE search and replace using string templating for a each file
+    Throws Exceptions if I/O fails or substitution is missing vars (safety check)
+    """
+    with open(path, "r+") as f:
+        f_content = apply_template(f.read(), branding_map)
+        f.seek(0)
+        f.write(f_content)
+        f.truncate()  # This removes any original content beyond the end of templated content
+
 
 def apply_templating_to_folder(path, branding_map):
-	""" Walks through all contents of folder recursively, and applies templating """	
-	for root, dirnames, filenames in os.walk(path):
-		for filename in filenames:
-			apply_templating_to_file(os.path.join(root, filename), branding_map)
+    """Walks through all contents of folder recursively, and applies templating"""
+    for root, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            apply_templating_to_file(os.path.join(root, filename), branding_map)
+
 
 # Importable as a library without executing it
-if(__name__ == '__main__'):
-	if (len(sys.argv) != 2):
-		raise Exception("Usage: branding.py [file or folder name]")
-	path = sys.argv[1]
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise Exception("Usage: branding.py [file or folder name]")
+    path = sys.argv[1]
 
-	mypath = os.path.dirname(os.path.realpath(__file__))
-	branding_values = read_branding_variables(mypath, BRANDING_ENV_VARIABLE_LIST, BRANDING_ENV_PATH_LIST)
+    mypath = os.path.dirname(os.path.realpath(__file__))
+    branding_values = read_branding_variables(
+        mypath, BRANDING_ENV_VARIABLE_LIST, BRANDING_ENV_PATH_LIST
+    )
 
-	# List of branding values that are allowed to be blank
-	allowed_blank = ['RELEASELINE']
-	# Remove branding values with nothing set so we fail early if they are used in a template
-	# except for values where blank is explicitly allows
-	branding_values = {k: v for k, v in branding_values.items() if k in allowed_blank or v}
+    # List of branding values that are allowed to be blank
+    allowed_blank = ["RELEASELINE"]
+    # Remove branding values with nothing set so we fail early if they are used in a template
+    # except for values where blank is explicitly allows
+    branding_values = {
+        k: v for k, v in branding_values.items() if k in allowed_blank or v
+    }
 
-	# Apply templating to files or content of folder
-	if os.path.isfile(path):
-		apply_templating_to_file(path, branding_values)
-	elif os.path.isdir(path):
-		apply_templating_to_folder(path, branding_values)
-	else:
-		raise Exception("Supplied path must be file or directory")
+    # Apply templating to files or content of folder
+    if os.path.isfile(path):
+        apply_templating_to_file(path, branding_values)
+    elif os.path.isdir(path):
+        apply_templating_to_folder(path, branding_values)
+    else:
+        raise Exception("Supplied path must be file or directory")
--- bin/test_branding.py
+++ bin/test_branding.py
@@ -16,7 +16,7 @@ SUMMARY
 AUTHOR
 # Not here
 LICENSE
- HOMEPAGE  
+ HOMEPAGE
 CHANGELOG_PAGE
 ----------------------------------
 
@@ -31,21 +31,34 @@ SHORT_VARS = """
 TESTVAR
 """
 
-# NOMATCH is a section that should *not* be treated as a variable for substitution to 
+# NOMATCH is a section that should *not* be treated as a variable for substitution to
 # Avoid issues with curly braces
-TEMPLATED_CONTENT = "My @@VAR@@ is @@FILECONTENT@@ and I have @@@@ESCAPES@@ and @@{NOMATCH}@@ stuff"
-TEMPLATE_VARS = {'VAR': 'SPECIAL', 'FILECONTENT': 'gooooooober'}
+TEMPLATED_CONTENT = (
+    "My @@VAR@@ is @@FILECONTENT@@ and I have @@@@ESCAPES@@ and @@{NOMATCH}@@ stuff"
+)
+TEMPLATE_VARS = {"VAR": "SPECIAL", "FILECONTENT": "gooooooober"}
 
 # Shows that substitutions were performed, and NOMATCH does not get handled as a substitution variable
-TEMPLATE_EXPECTED = 'My SPECIAL is gooooooober and I have @@ESCAPES@@ and @@{NOMATCH}@@ stuff'
+TEMPLATE_EXPECTED = (
+    "My SPECIAL is gooooooober and I have @@ESCAPES@@ and @@{NOMATCH}@@ stuff"
+)
+
 
 class TestBranding(unittest.TestCase):
-
     def test_clean_lines(self):
         cleaned = set(branding.clean_text_lines(RAW_CONTENT))
-        expected = {'PRODUCTNAME', 'ARTIFACTNAME', 'SUMMARY', 
-          'PORT', 'AUTHOR', 'LICENSE', 'HOMEPAGE', 'CHANGELOG_PAGE', 
-          'SUSE_URL', 'DEB_URL', 'LICENSE_TEXT'
+        expected = {
+            "PRODUCTNAME",
+            "ARTIFACTNAME",
+            "SUMMARY",
+            "PORT",
+            "AUTHOR",
+            "LICENSE",
+            "HOMEPAGE",
+            "CHANGELOG_PAGE",
+            "SUSE_URL",
+            "DEB_URL",
+            "LICENSE_TEXT",
         }
         invalid_results = cleaned ^ expected  # Items in one set but not the other
         self.assertFalse(invalid_results)
@@ -54,30 +67,32 @@ class TestBranding(unittest.TestCase):
         temp = tempfile.NamedTemporaryFile()
         temp.write(SHORT_VARS.encode())
         temp.seek(0)
-        os.environ['TESTVAR']='testvalue'
+        os.environ["TESTVAR"] = "testvalue"
         vals = branding.read_env_variable_list(temp.name)
         self.assertEqual(1, len(vals))
-        self.assertEqual('testvalue', vals['TESTVAR'])
-    
+        self.assertEqual("testvalue", vals["TESTVAR"])
+
     def test_read_file_content(self):
-        """ Write file and then read, using path from env """
+        """Write file and then read, using path from env"""
         temp = tempfile.NamedTemporaryFile()
         temp.write(RAW_CONTENT.encode())
         temp.seek(0)
 
-        os.environ['FILEPATH']=temp.name
-        vals = branding.read_file_content({'FILEPATH': temp.name})
-        self.assertEqual(RAW_CONTENT, vals['FILEPATH'])
+        os.environ["FILEPATH"] = temp.name
+        vals = branding.read_file_content({"FILEPATH": temp.name})
+        self.assertEqual(RAW_CONTENT, vals["FILEPATH"])
 
     def test_templating(self):
         output = branding.apply_template(TEMPLATED_CONTENT, TEMPLATE_VARS)
         self.assertEqual(TEMPLATE_EXPECTED, output)
 
     def test_missing_variable(self):
-        """ Prove branding will fail if branding variable is undefined """
+        """Prove branding will fail if branding variable is undefined"""
 
         try:
-            output = branding.apply_template('My @@UNDEFINED_VALUE@@ is going to fail', {'going': 'to fail'})
+            output = branding.apply_template(
+                "My @@UNDEFINED_VALUE@@ is going to fail", {"going": "to fail"}
+            )
             self.fail("Should throw KeyError")
         except KeyError:
             pass
@@ -91,5 +106,5 @@ class TestBranding(unittest.TestCase):
         self.assertEqual(TEMPLATE_EXPECTED, temp.read().decode())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
