7334ec809dfbad890530b5d3b47349c6dcbe41a8
Basil Crow <me@basilcrow.com>
2022-02-02 15:45:56 -0800

    Handle single quotes

--- systemd/migrate.sh
+++ systemd/migrate.sh
@@ -56,48 +56,48 @@ read_old_options() {
 		if [ -n "${NAME}" ]; then
 			# For deb, these are all the arguments, except for the JENKINS_ENABLE_ACCESS_LOG additions
 			webroot_quoted=false
-			TMP_JENKINS_WEBROOT=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--webroot="\([[:alnum:][:space:]+,-\./:;@\_]*\)".*/\1/p')
+			TMP_JENKINS_WEBROOT="$(printf '%s' "${JENKINS_ARGS}" | sed -n "s/.*--webroot=[\"']\\([[:alnum:][:space:]+,-\\./:;@\\_]*\\)[\"'].*/\\1/p")"
 			[ -n "${TMP_JENKINS_WEBROOT}" ] && webroot_quoted=true
-			[ -z "${TMP_JENKINS_WEBROOT}" ] && TMP_JENKINS_WEBROOT=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--webroot=\([[:alnum:][:punct:]]*\).*/\1/p')
+			[ -z "${TMP_JENKINS_WEBROOT}" ] && TMP_JENKINS_WEBROOT="$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--webroot=\([[:alnum:][:punct:]]*\).*/\1/p')"
 
 			log_quoted=false
-			TMP_JENKINS_LOG=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--logfile="\([[:alnum:][:space:]+,-\./:;@\_]*\)".*/\1/p')
+			TMP_JENKINS_LOG="$(printf '%s' "${JENKINS_ARGS}" | sed -n "s/.*--logfile=[\"']\\([[:alnum:][:space:]+,-\\./:;@\\_]*\\)[\"'].*/\\1/p")"
 			[ -n "${TMP_JENKINS_LOG}" ] && log_quoted=true
-			[ -z "${TMP_JENKINS_LOG}" ] && TMP_JENKINS_LOG=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--logfile=\([[:alnum:][:punct:]]*\).*/\1/p')
+			[ -z "${TMP_JENKINS_LOG}" ] && TMP_JENKINS_LOG="$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--logfile=\([[:alnum:][:punct:]]*\).*/\1/p')"
 
-			TMP_JENKINS_PORT=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--httpPort=\([[:alnum:][:punct:]]*\).*/\1/p')
+			TMP_JENKINS_PORT="$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--httpPort=\([[:alnum:][:punct:]]*\).*/\1/p')"
 
-			TMP_JENKINS_LISTEN_ADDRESS=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--httpListenAddress=\([[:alnum:][:punct:]]*\).*/\1/p')
+			TMP_JENKINS_LISTEN_ADDRESS="$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--httpListenAddress=\([[:alnum:][:punct:]]*\).*/\1/p')"
 
-			TMP_JENKINS_HTTPS_PORT=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--httpsPort=\([[:alnum:][:punct:]]*\).*/\1/p')
+			TMP_JENKINS_HTTPS_PORT="$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--httpsPort=\([[:alnum:][:punct:]]*\).*/\1/p')"
 
-			TMP_JENKINS_HTTPS_LISTEN_ADDRESS=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--httpsListenAddress=\([[:alnum:][:punct:]]*\).*/\1/p')
+			TMP_JENKINS_HTTPS_LISTEN_ADDRESS="$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--httpsListenAddress=\([[:alnum:][:punct:]]*\).*/\1/p')"
 
 			keystore_quoted=false
-			TMP_JENKINS_HTTPS_KEYSTORE=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--httpsKeyStore="\([[:alnum:][:space:]+,-\./:;@\_]*\)".*/\1/p')
+			TMP_JENKINS_HTTPS_KEYSTORE="$(printf '%s' "${JENKINS_ARGS}" | sed -n "s/.*--httpsKeyStore=[\"']\\([[:alnum:][:space:]+,-\\./:;@\\_]*\\)[\"'].*/\\1/p")"
 			[ -n "${TMP_JENKINS_HTTPS_KEYSTORE}" ] && keystore_quoted=true
-			[ -z "${TMP_JENKINS_HTTPS_KEYSTORE}" ] && TMP_JENKINS_HTTPS_KEYSTORE=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--httpsKeyStore=\([[:alnum:][:punct:]]*\).*/\1/p')
+			[ -z "${TMP_JENKINS_HTTPS_KEYSTORE}" ] && TMP_JENKINS_HTTPS_KEYSTORE="$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--httpsKeyStore=\([[:alnum:][:punct:]]*\).*/\1/p')"
 
 			keystore_password_quoted=false
-			TMP_JENKINS_HTTPS_KEYSTORE_PASSWORD=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--httpsKeyStorePassword="\([[:alnum:][:space:]+,-\./:;@\_]*\)".*/\1/p')
+			TMP_JENKINS_HTTPS_KEYSTORE_PASSWORD="$(printf '%s' "${JENKINS_ARGS}" | sed -n "s/.*--httpsKeyStorePassword=[\"']\\([[:alnum:][:space:]+,-\\./:;@\\_]*\\)[\"'].*/\\1/p")"
 			[ -n "${TMP_JENKINS_HTTPS_KEYSTORE_PASSWORD}" ] && keystore_password_quoted=true
-			[ -z "${TMP_JENKINS_HTTPS_KEYSTORE_PASSWORD}" ] && TMP_JENKINS_HTTPS_KEYSTORE_PASSWORD=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--httpsKeyStorePassword=\([[:alnum:][:punct:]]*\).*/\1/p')
+			[ -z "${TMP_JENKINS_HTTPS_KEYSTORE_PASSWORD}" ] && TMP_JENKINS_HTTPS_KEYSTORE_PASSWORD="$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--httpsKeyStorePassword=\([[:alnum:][:punct:]]*\).*/\1/p')"
 
-			TMP_JENKINS_HTTP2_PORT=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--http2Port=\([[:alnum:][:punct:]]*\).*/\1/p')
+			TMP_JENKINS_HTTP2_PORT="$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--http2Port=\([[:alnum:][:punct:]]*\).*/\1/p')"
 
-			TMP_JENKINS_HTTP2_LISTEN_ADDRESS=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--http2ListenAddress=\([[:alnum:][:punct:]]*\).*/\1/p')
+			TMP_JENKINS_HTTP2_LISTEN_ADDRESS="$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--http2ListenAddress=\([[:alnum:][:punct:]]*\).*/\1/p')"
 
-			TMP_JENKINS_DEBUG_LEVEL=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--debug=\([[:alnum:][:punct:]]*\).*/\1/p')
+			TMP_JENKINS_DEBUG_LEVEL="$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--debug=\([[:alnum:][:punct:]]*\).*/\1/p')"
 
 			extra_lib_folder_quoted=false
-			TMP_JENKINS_EXTRA_LIB_FOLDER=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--extraLibFolder="\([[:alnum:][:space:]+,-\./:;@\_]*\)".*/\1/p')
+			TMP_JENKINS_EXTRA_LIB_FOLDER="$(printf '%s' "${JENKINS_ARGS}" | sed -n "s/.*--extraLibFolder=[\"']\\([[:alnum:][:space:]+,-\\./:;@\\_]*\\)[\"'].*/\\1/p")"
 			[ -n "${TMP_JENKINS_EXTRA_LIB_FOLDER}" ] && extra_lib_folder_quoted=true
-			[ -z "${TMP_JENKINS_EXTRA_LIB_FOLDER}" ] && TMP_JENKINS_EXTRA_LIB_FOLDER=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--extraLibFolder=\([[:alnum:][:punct:]]*\).*/\1/p')
+			[ -z "${TMP_JENKINS_EXTRA_LIB_FOLDER}" ] && TMP_JENKINS_EXTRA_LIB_FOLDER="$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--extraLibFolder=\([[:alnum:][:punct:]]*\).*/\1/p')"
 
 			prefix_quoted=false
-			TMP_PREFIX=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--prefix="\([[:alnum:][:space:]+,-\./:;@\_]*\)".*/\1/p')
+			TMP_PREFIX="$(printf '%s' "${JENKINS_ARGS}" | sed -n "s/.*--prefix=[\"']\\([[:alnum:][:space:]+,-\\./:;@\\_]*\\)[\"'].*/\\1/p")"
 			[ -n "${TMP_PREFIX}" ] && prefix_quoted=true
-			[ -z "${TMP_PREFIX}" ] && TMP_PREFIX=$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--prefix=\([[:alnum:][:punct:]]*\).*/\1/p')
+			[ -z "${TMP_PREFIX}" ] && TMP_PREFIX="$(printf '%s' "${JENKINS_ARGS}" | sed -n 's/.*--prefix=\([[:alnum:][:punct:]]*\).*/\1/p')"
 
 			[ -n "${TMP_JENKINS_WEBROOT}" ] && JENKINS_WEBROOT="${TMP_JENKINS_WEBROOT}"
 			[ -n "${TMP_JENKINS_LOG}" ] && JENKINS_LOG="${TMP_JENKINS_LOG}"
@@ -115,59 +115,61 @@ read_old_options() {
 			[ -n "${TMP_PREFIX}" ] && has_prefix=true
 
 			if $webroot_quoted; then
-				JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--webroot="[[:alnum:][:space:]+,-\./:;@\_]*"//g')
+				JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed "s/--webroot=[\"'][[:alnum:][:space:]+,-\\./:;@\\_]*[\"']//g")"
 			else
-				JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--webroot=[[:alnum:][:punct:]]*//g')
+				JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/--webroot=[[:alnum:][:punct:]]*//g')"
 			fi
 
 			if $log_quoted; then
-				JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--logfile="[[:alnum:][:space:]+,-\./:;@\_]*"//g')
+				JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed "s/--logfile=[\"'][[:alnum:][:space:]+,-\\./:;@\\_]*[\"']//g")"
 			else
-				JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--logfile=[[:alnum:][:punct:]]*//g')
+				JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/--logfile=[[:alnum:][:punct:]]*//g')"
 			fi
 
-			JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--httpPort=[[:alnum:][:punct:]]*//g')
+			JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/--httpPort=[[:alnum:][:punct:]]*//g')"
 
-			JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--httpListenAddress=[[:alnum:][:punct:]]*//g')
+			JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/--httpListenAddress=[[:alnum:][:punct:]]*//g')"
 
-			JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--httpsPort=[[:alnum:][:punct:]]*//g')
+			JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/--httpsPort=[[:alnum:][:punct:]]*//g')"
 
-			JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--httpsListenAddress=[[:alnum:][:punct:]]*//g')
+			JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/--httpsListenAddress=[[:alnum:][:punct:]]*//g')"
 
 			if $keystore_quoted; then
-				JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--httpsKeyStore="[[:alnum:][:space:]+,-\./:;@\_]*"//g')
+				JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed "s/--httpsKeyStore=[\"'][[:alnum:][:space:]+,-\\./:;@\\_]*[\"']//g")"
 			else
-				JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--httpsKeyStore=[[:alnum:][:punct:]]*//g')
+				JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/--httpsKeyStore=[[:alnum:][:punct:]]*//g')"
 			fi
 
 			if $keystore_password_quoted; then
-				JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--httpsKeyStorePassword="[[:alnum:][:space:]+,-\./:;@\_]*"//g')
+				JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed "s/--httpsKeyStorePassword=[\"'][[:alnum:][:space:]+,-\\./:;@\\_]*[\"']//g")"
 			else
-				JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--httpsKeyStorePassword=[[:alnum:][:punct:]]*//g')
+				JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/--httpsKeyStorePassword=[[:alnum:][:punct:]]*//g')"
 			fi
 
-			JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--http2Port=[[:alnum:][:punct:]]*//g')
+			JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/--http2Port=[[:alnum:][:punct:]]*//g')"
 
-			JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--http2ListenAddress=[[:alnum:][:punct:]]*//g')
+			JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/--http2ListenAddress=[[:alnum:][:punct:]]*//g')"
 
-			JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--debug=[[:alnum:][:punct:]]*//g')
+			JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/--debug=[[:alnum:][:punct:]]*//g')"
 
 			if $extra_lib_folder_quoted; then
-				JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--extraLibFolder="[[:alnum:][:space:]+,-\./:;@\_]*"//g')
+				JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed "s/--extraLibFolder=[\"'][[:alnum:][:space:]+,-\\./:;@\\_]*[\"']//g")"
 			else
-				JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--extraLibFolder=[[:alnum:][:punct:]]*//g')
+				JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/--extraLibFolder=[[:alnum:][:punct:]]*//g')"
 			fi
 
 			if $prefix_quoted; then
-				JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--prefix="[[:alnum:][:space:]+,-\./:;@\_]*"//g')
+				JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed "s/--prefix=[\"'][[:alnum:][:space:]+,-\\./:;@\\_]*[\"']//g")"
 			else
-				JENKINS_ARGS=$(printf '%s' "${JENKINS_ARGS}" | sed 's/--prefix=[[:alnum:][:punct:]]*//g')
+				JENKINS_ARGS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/--prefix=[[:alnum:][:punct:]]*//g')"
 			fi
 
 			# All that remains are the extra arguments
+			NEW_JENKINS_OPTS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/^[[:space:]]*//g')"
 			NEW_JENKINS_OPTS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/[[:space:]]*$//g')"
 		else
 			# For rpm and suse, these are extra arguments
+			NEW_JENKINS_OPTS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/^[[:space:]]*//g')"
 			NEW_JENKINS_OPTS="$(printf '%s' "${JENKINS_ARGS}" | sed 's/[[:space:]]*$//g')"
 		fi
 	fi
