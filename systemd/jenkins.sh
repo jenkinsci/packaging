b1885c2cf99564e45299cf6901d5639a89825eba
Basil Crow <me@basilcrow.com>
2022-09-09 10:49:13 -0700

    [JENKINS-69570] Java version check incorrect in init.d/jenkins

--- systemd/jenkins.sh
+++ systemd/jenkins.sh
@@ -41,7 +41,7 @@ check_java_version() {
 
 	if [ -z "${java_version}" ]; then
 		return 1
-	elif [ "${java_version}" != "17" ] && [ "${java_version}" != "11" ] && [ "${java_version}" != "1.8" ]; then
+	elif [ "${java_version}" != "17" ] && [ "${java_version}" != "11" ]; then
 		return 1
 	else
 		return 0
