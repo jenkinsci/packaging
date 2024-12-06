5f5a07264c489c0b8ae56303b1eeee2bb43b628c
Alex Earl <slide.o.mix@gmail.com>
2024-12-05 21:16:13 -0700

    Fixup request string

--- prep.ps1
+++ prep.ps1
@@ -3,7 +3,7 @@ Set-Location $PSScriptRoot
 
 if(-not (Test-Path -Path $env:WAR)) {
     $latestJson = (Invoke-WebRequest -Uri "https://api.github.com/repos/jenkins-infra/jenkins-version/releases/latest" -UseBasicParsing).Content | ConvertFrom-Json
-    Invoke-WebRequest -Uri "https://github.com/jenkins-infra/jenkins-version/releases/download/${latestJson.name}/jenkins-version-windows-amd64.zip" -OutFile (Join-Path $PSScriptRoot "jenkins-version-windows-amd64.zip") -UseBasicParsing
+    Invoke-WebRequest -Uri ("https://github.com/jenkins-infra/jenkins-version/releases/download/{0}/jenkins-version-windows-amd64.zip" -f $latestJson.name) -OutFile (Join-Path $PSScriptRoot "jenkins-version-windows-amd64.zip") -UseBasicParsing
     [IO.Compression.ZipFile]::ExtractToDirectory('jenkins-version-windows-amd64.zip', $PSScriptRoot)
     & .\jv.exe download
     Remove-Item -Force -Path 'jv.exe','README.md','LICENSE','jenkins-version-windows-amd64.zip'
