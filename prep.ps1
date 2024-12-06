a67dea98530aa5932b37bea9e9d3d7c13b8adc96
Alex Earl <slide.o.mix@gmail.com>
2024-12-05 21:10:43 -0700

    Forgot UseBasicParsing

--- prep.ps1
+++ prep.ps1
@@ -2,7 +2,7 @@
 Set-Location $PSScriptRoot
 
 if(-not (Test-Path -Path $env:WAR)) {
-    $latestJson = (Invoke-WebRequest -Uri "https://api.github.com/repos/jenkins-infra/jenkins-version/releases/latest").Content | ConvertFrom-Json
+    $latestJson = (Invoke-WebRequest -Uri "https://api.github.com/repos/jenkins-infra/jenkins-version/releases/latest" -UseBasicParsing).Content | ConvertFrom-Json
     Invoke-WebRequest -Uri "https://github.com/jenkins-infra/jenkins-version/releases/download/${latestJson.name}/jenkins-version-windows-amd64.zip" -OutFile (Join-Path $PSScriptRoot "jenkins-version-windows-amd64.zip") -UseBasicParsing
     [IO.Compression.ZipFile]::ExtractToDirectory('jenkins-version-windows-amd64.zip', $PSScriptRoot)
     & .\jv.exe download
