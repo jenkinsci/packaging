c243978fca3cdee6d153333da5a3ef05dea62781
Alex Earl <slide.o.mix@gmail.com>
2024-12-05 20:53:41 -0700

    Go back to Invoke-WebRequest

--- prep.ps1
+++ prep.ps1
@@ -1,10 +1,8 @@
 
 Set-Location $PSScriptRoot
 
-$PSVersionTable
-
 if(-not (Test-Path -Path $env:WAR)) {
-    curl -LO "https://github.com/jenkins-infra/jenkins-version/releases/download/latest/jenkins-version-windows-amd64.zip"
+    Invoke-WebRequest -Uri "https://github.com/jenkins-infra/jenkins-version/releases/download/latest/jenkins-version-windows-amd64.zip" -OutFile (Join-Path $PSScriptRoot 'jenkins-version-windows-amd64.zip') -UseBasicParsing
     [IO.Compression.ZipFile]::ExtractToDirectory('jenkins-version-windows-amd64.zip', $PSScriptRoot)
     & .\jv.exe download
     Remove-Item -Force -Path 'jv.exe','README.md','LICENSE','jenkins-version-windows-amd64.zip'
