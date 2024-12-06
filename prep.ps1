f226a742cb34587702e421c63e485cddbdd80475
Alex Earl <slide.o.mix@gmail.com>
2024-12-05 20:49:25 -0700

    Print powershell version

--- prep.ps1
+++ prep.ps1
@@ -1,6 +1,8 @@
 
 Set-Location $PSScriptRoot
 
+$PSVersionTable
+
 if(-not (Test-Path -Path $env:WAR)) {
     curl -LO "https://github.com/jenkins-infra/jenkins-version/releases/download/latest/jenkins-version-windows-amd64.zip"
     [IO.Compression.ZipFile]::ExtractToDirectory('jenkins-version-windows-amd64.zip', $PSScriptRoot)
