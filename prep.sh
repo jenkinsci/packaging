5f2a9b3f22dd581327f4ed4c364789211b8b50f8
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2026-01-06 21:02:19 +0100

    hotfix(backport): hardcode safe weekly version 2.541 instead of retrieving latest weekly 2.545
    Molecule tests are currently failing on latest weekly 2.545 while they passed on the previous 2.544 one. Using the LTS base version for this backport.

--- prep.sh
+++ prep.sh
@@ -8,14 +8,23 @@ cd "$(dirname "$0")"
 # releases, ideally implemented in the Makefile. Then both this repository and
 # jenkins-infra/release should be refactored to consume the new functionality.
 
+# Harcoding a safe Weekly version to 2.541 in this stable-2.541 branch for 2.541 LTS as 2.545 doesn't currently pass tests
+safeWeeklyVersion="2.541"
+
 if [[ ! -f "$WAR" ]]; then
-	# jv utilizes the JENKINS_VERSION environment variable which can be the line (latest/weekly/lts/stable) or an exact version
-	jv download
+	# # jv utilizes the JENKINS_VERSION environment variable which can be the line (latest/weekly/lts/stable) or an exact version
+	# jv download --version-identifier "${jenkinsVersion}"
+
+	# Using hardcoded safe version for this stable branch
+	jv download --version-identifier "${safeWeeklyVersion}"
 fi
 
 if [[ ! -f "${WAR}.asc" ]]; then
-	# jv utilizes the JENKINS_VERSION environment variable which can be the line (latest/weekly/lts/stable) or an exact version
-	jenkinsVersion="$(jv get)"
+	# # jv utilizes the JENKINS_VERSION environment variable which can be the line (latest/weekly/lts/stable) or an exact version
+	# jenkinsVersion="$(jv get)"
+
+	# Using hardcoded safe version for this stable branch
+	jenkinsVersion="${safeWeeklyVersion}"
 
 	# Download signature from Artifactory (signed by Maven during the release process)
 	# TODO: switch to get.jenkins.io once https://github.com/jenkins-infra/helpdesk/issues/4055 is finished
